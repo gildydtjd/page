837c8f471b729babfafe13a474dd1240c9bea761
dragon <gildydtjd@naver.com>
2020-06-03 00:18:08 +0900

    parallex 시작

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './header/Header';
-
+import Body from './body/Body';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }

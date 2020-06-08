cffd8465e3a645da3caaa800276013198f6a69da
dragon <gildydtjd@naver.com>
2020-06-08 17:34:34 +0900

    내용추가

--- src/App.js
+++ src/App.js
@@ -17,40 +17,59 @@ function App() {
     transform : 'translate(-50%, -50%)',
     fontSize : '30px',
   }
+  const size = {
+      width: window.innerWidth || document.body.clientWidth,
+      height: window.innerHeight || document.body.clientHeight
+    }
+
   return (
     <div className="App">
       <Header />
       <Parallax bgImage={image1} strength={600}>
-        <div style={{ height : 700 }}>
+        <div style={{ minHeight: '100vh' }}>
           <div style={inlineStyle}>Web Portfolio</div>
         </div>
       </Parallax>
 
-      <h1></h1>
+      <div style={{ display : 'flex' ,minHeight: '100vh' , borderSpacing : '0' }}>
+        <div style={{ minHeight: '100vh' , width : size.width/2 , backgroundColor : '#0067a3'}}>
+          <h2 style={{textAlign: 'right' , padding : '10%'}}>
+            <span style={{ display: 'block', fontSize: '24px',  opacity: '.5', speak: 'none' }}>Intro</span>
+            <span style={{ display: 'block', fontSize: '64px', paddingBottom: '.25em', position : 'relative'}}>About myself</span>
+          </h2>
+          <div style={{ marginLeft: 'auto' , maxwidth: '35em'}} >
+          <p style={{marginBlockStart: '1em', marginBlockEnd: '1em', marginInlineStart: '0px', marginInlineEnd: '0px'}}> 웹개발을 시작한 경기도의 아들 28살 길용성입니다.</p>
+
+          </div>
+        </div>
+
+        <div style={{ minHeight: '100vh' , width : size.width/2 , backgroundColor : 'black' }}>
+        <h2 style={{textAlign: 'left' , padding : '10%'}}>
+            <span style={{ display: 'block', fontSize: '24px',  opacity: '.5', speak: 'none' }}>Skills</span>
+            <span style={{ display: 'block', fontSize: '64px', paddingBottom: '.25em', position : 'relative'}}>What I can do</span>
+          </h2>
+        </div>
+      </div>
+
       <Parallax bgImage={image2} blur={{ min: -1, max: 5 }}>
         <div style={{ height : 700 }}>
           <div style={inlineStyle}>Dynamic blur</div>
         </div>
       </Parallax>
-      <h1></h1>
 
       <Parallax bgImage={image3} strength={-200}>
         <div style={{ height : 700 }}>
           <div style={inlineStyle}>Reverse direction</div>
         </div>
       </Parallax>
-      <h1></h1>
 
-      <Parallax 
-        bgImage={image4} strength={200} >
+      <Parallax bgImage={image4} strength={200} >
         <div style={{ height : 700 }}>
           <div style={inlineStyle}>Render prop</div>
         </div>
       </Parallax>
-      <h1></h1>
 
       <div style={{ height : '100vh' }}></div>
-
     </div>
   );
 }
--- src/header/headerContents/HeaderMenu.js
+++ src/header/headerContents/HeaderMenu.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 export default function HeaderMenu() {
+  const img1 = "./Img/sns.jpg";
+  const img2 = "./Img/user.png";
   return (
     <div className="header-menu">
       <ul className="header-1">
-        <li><a><img className="sns" src="./Img/sns.jpg"></img></a></li>
-        <li className="li-menu"><a className="header-top-menu">BLOG</a></li>
+        <li><a href='/'><img className="sns" src= {img1}></img></a></li>
+        <li className="li-menu"><a href='/' className="header-top-menu">BLOG</a></li>
       </ul>
       <ul className="header-2">
-        <li><a><img className="user" src="./Img/user.png"></img></a></li>
-        <li><a className="header-top-menu">JOIN US</a></li>
-        <li><a className="header-top-menu">LOGIN</a></li>
+        <li><a href='/'><img className="user" src= {img2}></img></a></li>
+        <li><a href='/' className="header-top-menu">JOIN US</a></li>
+        <li><a href='/' className="header-top-menu">LOGIN</a></li>
         </ul>
     </div>
   )
